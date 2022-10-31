f5aae130818f72912acc41a0ed35bda117d470f4
Wellington Souza <wellington.souza.pb@compasso.com.br>
2022-10-31 03:36:28 -0300

    -- Menu item component

---
+++ app/src/js/components/MenuItem.stories.ts
@@ -0,0 +1,54 @@
+import { Story, Meta } from "@storybook/html";
+import { createMenuItem, MenuItemProps } from "./MenuItem";
+
+export default {
+  title: "Components/MenuItem",
+
+  argTypes: {
+    label: { control: "text" },
+    iconName: {
+      control: { type: "select" },
+      options: [
+        "logo",
+        "home",
+        "explore",
+        "notifications",
+        "messages",
+        "bookmarks",
+        "lists",
+        "profile",
+        "more",
+      ],
+    },
+    linkURL: { control: "text" },
+    dark: { control: "boolean" },
+    selected: { control: "boolean" },
+  },
+} as Meta;
+
+const Template: Story<MenuItemProps> = (args) => {
+  return createMenuItem(args);
+};
+
+export const menuItem = Template.bind({});
+menuItem.args = {
+  label: "Home",
+  iconName: "home",
+  linkURL: "/404.html",
+};
+
+export const menuItemDark = Template.bind({});
+menuItemDark.args = {
+  label: "Home",
+  iconName: "home",
+  linkURL: "/404.html",
+  dark: true,
+};
+
+export const menuItemSelected = Template.bind({});
+menuItemSelected.args = {
+  label: "Home",
+  iconName: "home",
+  linkURL: "/404.html",
+  selected: true,
+};
---
+++ app/src/js/components/MenuItem.ts
@@ -0,0 +1,56 @@
+import "./../../../dist/styles/main.css";
+
+export interface MenuItemProps {
+  label: string;
+  iconName:
+    | "logo"
+    | "home"
+    | "explore"
+    | "notifications"
+    | "messages"
+    | "bookmarks"
+    | "lists"
+    | "profile"
+    | "more";
+  linkURL?: string;
+  dark?: boolean;
+  selected?: boolean;
+}
+
+export const createMenuItem = ({
+  label,
+  iconName,
+  linkURL,
+  dark,
+  selected,
+}: MenuItemProps) => {
+  let menuItem: string = ``;
+  if (selected) {
+    menuItem = `
+    <li class="menu-item${selected ? " menu-selected" : ""}">
+      <a href="${linkURL ? linkURL : ""}">
+        <picture class="image-container">
+          <img src="/images/icon-${iconName}${
+      selected ? "-selected" : ""
+    }.png" alt="${iconName} menu icon">
+        </picture>
+        <span class="label">${label}</span>
+      </a>
+    </li>
+    `;
+    return menuItem;
+  }
+  menuItem = `
+    <li class="menu-item">
+      <a href="${linkURL ? linkURL : ""}">
+        <picture class="image-container">
+          <img src="/images/icon-${iconName}${
+    dark ? "-dark" : ""
+  }.png" alt="${iconName} menu icon">
+        </picture>
+        <span class="label">${label}</span>
+      </a>
+    </li>
+    `;
+  return menuItem;
+};
