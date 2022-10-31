f1c171096dc8af9cde47b43419b3180495c146fa
Wellington Souza <wellington.souza.pb@compasso.com.br>
2022-10-31 08:45:32 -0300

    -- Avatar component

---
+++ app/src/js/components/Avatar.stories.ts
@@ -0,0 +1,31 @@
+import { Story, Meta } from "@storybook/html";
+import { createAvatar, AvatarProps } from "./Avatar";
+
+export default {
+  title: "Components/Avatar",
+
+  argTypes: {
+    size: {
+      control: { type: "select" },
+      options: ["small", "medium"],
+    },
+    userID: { control: "text" },
+    linkURL: { control: "text" },
+  },
+} as Meta;
+
+const Template: Story<AvatarProps> = (args: any) => {
+  return createAvatar(args);
+};
+
+export const avatarSmall = Template.bind({});
+avatarSmall.args = {
+  size: "small",
+  linkURL: "/404.html",
+};
+
+export const avatarMedium = Template.bind({});
+avatarMedium.args = {
+  size: "medium",
+  linkURL: "/404.html",
+};
